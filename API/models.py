a61996054689449088818edbdb952c14ca7053b8
shine chikwapulo <chikwapuloshine@gmail.com>
2024-05-16 02:04:19 +0200

    complete👾

--- API/models.py
+++ API/models.py
@@ -16,8 +16,9 @@ def generate_unique_code():
 
 class Room(models.Model):
     code = models.CharField(
-        max_length=8, default=generate_unique_code, unique=True)
+    max_length=8, default=generate_unique_code, unique=True)
     host = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    current_song = models.CharField(max_length=50, null=True)
